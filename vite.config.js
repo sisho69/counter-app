6f6ab5efe6c1b85b80bbdd24105d4c167d2a5fc4
sisho69 <95492257+sisho69@users.noreply.github.com>
2025-10-23 00:17:46 +0900

    local

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/counter-app',
+  base: '/counter-app/',
   plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
